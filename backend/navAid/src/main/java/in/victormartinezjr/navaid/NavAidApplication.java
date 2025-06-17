80e38a2321300cec512960be5515a1ff7a71a981
Victor <victorm.martinezjr@outlook.com>
2025-06-17 13:51:28 -0400

    initalize project

---
+++ backend/navAid/src/main/java/in/victormartinezjr/navaid/NavAidApplication.java
@@ -0,0 +1,13 @@
+package in.victormartinezjr.navaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavAidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NavAidApplication.class, args);
+    }
+
+}
---
+++ backend/navAid/src/test/java/in/victormartinezjr/navaid/NavAidApplicationTests.java
@@ -0,0 +1,13 @@
+package in.victormartinezjr.navaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavAidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
